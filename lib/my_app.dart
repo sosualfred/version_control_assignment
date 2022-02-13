d2c3187b1b2789d8831dac11a48da3002de9617d
Elias Bansi <slimsavern@gmail.com>
2022-02-13 12:26:56 +0000

    Changed Color to Red

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -11,8 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
       home: const MyHomePage(),
     );
--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -13,6 +13,11 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text("Assignment"),
+      ),
       body: Center(
         child: Text("Hello Sogo"),
       ),
